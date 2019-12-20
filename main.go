bc68d0fc62a58ef9bdc9717cf15a5de33c6a5b41
pezhman Kasraee Job <pezhman.kasraee@softhouse.se>
2019-12-20 10:28:21 +0100

    add Three forked

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import "fmt"
 
-func main(){
-     fmt.Println("Hello ONE")
-     fmt.Println("Hello TWO")
+func main() {
+	fmt.Println("Hello ONE")
+	fmt.Println("Hello TWO")
+	fmt.Println("Hello, Three by pezhmankasraeejob")
 }
